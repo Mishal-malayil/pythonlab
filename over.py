17b451f7313c47cf799470f3c256662efb105487
Mishal-malayil <mishalmalayil2003@gmail.com>
2025-09-19 09:44:06 +0530

    done

---
+++ over.py
@@ -0,0 +1,10 @@
+n=input("Enter a list of integers:")
+numberlist=n.split()
+result=[]
+for num in numberlist:
+    n=int(num)
+    if n>100:
+        result.append("over")
+    else:
+        result.append(n)
+print(f'Processed list:{result}')        
