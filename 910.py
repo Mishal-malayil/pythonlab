2d648065e4d81c77c2d453479941b7ea3d4a8a29
Mishal-malayil <mishalsonu2003@gmail.com>
2025-10-15 12:02:18 +0530

    done

---
+++ 910.py
@@ -0,0 +1,8 @@
+color_list1=input("Enter color for list 1:").splict()
+color_list2=input("Enter color for list 2:").splict()
+
+set1=set(color_list1)
+set2=set(color_list2)
+result=set1-set2
+print("Result:",result)
+
