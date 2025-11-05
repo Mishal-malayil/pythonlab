c420658cecb314a6f58a7215046ecb691744a983
Mishal-malayil <mishalsonu2003@gmail.com>
2025-11-05 14:49:43 +0530

    completed

---
+++ q8_4.py
@@ -0,0 +1,11 @@
+def fact(n):
+    f = 1
+    for i in range(1, n + 1):
+        f *= i
+    return f
+n = int(input("Enter number of terms: "))
+s = 0
+for i in range(1, n + 1):
+    s += (i**3) / fact(i)
+
+print("Sum =", s)
